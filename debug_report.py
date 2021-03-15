0adef43ae1ae5dff8678dfe63d3635fe9ad048e3
donghaozhang <zdhpeter1991@gmail.com>
2021-03-15 15:56:21 +1100

    remove debuging files

--- debug_report.py
+++
@@ -1,9 +0,0 @@
-import json
-ann_path = '/media/hdd/donghao/imcaption/R2Gen/data/mimic_cxr/annotation.json'
-report = json.loads(open(ann_path, 'r').read())
-print(report.keys())
-print(report['train'][0])
-# self.examples = self.ann[self.split]
-# for i in range(len(self.examples)):
-#     self.examples[i]['ids'] = tokenizer(self.examples[i]['report'])[:self.max_seq_length]
-#     self.examples[i]['mask'] = [1] * len(self.examples[i]['ids'])
--- debug_reshape.py
+++
@@ -1,4 +0,0 @@
-import torch
-a = torch.arange(4)
-b = torch.reshape(a, (2, 1, 2))
-print(b.size())
