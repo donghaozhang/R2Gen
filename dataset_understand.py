984ac9291630776492a0f4347c5948b1d42c67b9
donghaozhang <zdhpeter1991@gmail.com>
2021-03-17 11:06:09 +1100

    work on storing csv into the dictionary data structure

--- dataset_understand.py
+++ dataset_understand.py
@@ -1,9 +1,11 @@
-cleaned_report_path = '/media/hdd/data/imcaption/danli_datav2/cleaned.csv'
 import csv
-
+import os
+cleaned_report_path = '/media/hdd/data/imcaption/danli_datav2/cleaned.csv'
+img_folder_path = '/media/hdd/data/imcaption/danli_datav2/images819'
 with open(cleaned_report_path) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
+    cleaned_csv_dict = {}
     for row in csv_reader:
         single_dic = {}
         if line_count == 0:
@@ -12,9 +14,24 @@ with open(cleaned_report_path) as csv_file:
             line_count += 1
         else:
             # print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-            line_count += 1
-            single_dic['id'] = row[0]
             
+            single_dic['id'] = row[0]
+            single_dic['text'] = row[1]
+            single_dic['pred_modality'] = row[2]
+            single_dic['DS'] = row[3]
+            single_dic['Ds'] = row[4]
+            single_dic['LS'] = row[5]
+            single_dic['ls'] = row[6]
+            single_dic['dir'] = os.path.join(img_folder_path, os.path.basename(row[7]))
+            single_dic['caption'] = row[8]
+            cleaned_csv_dict[line_count] = single_dic
+            line_count += 1
     print(f'Processed {line_count} lines.')
-print('the title is', column_title)
-print('row[0]', row[0], 'row[1]', row[1], 'row[2]', row[2], 'row[3]', row[3], 'row[4]', row[4], 'row[5]', row[5])
+# print('the title is', column_title)
+# print(img_folder_path, os.path.basename(single_dic['dir']))
+# print(cleaned_csv_dict[1])
+# cur_dict = single_dic.copy()
+print('cur_dict[dir]', single_dic['dir'])
+print(single_dic)
+# print('row[0]', row[0], 'row[1]', row[1], 'row[2]', row[2], 'row[3]', row[3], 'row[4]', row[4], 'row[5]', row[5])
+
