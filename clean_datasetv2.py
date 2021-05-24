a77992d4b91cc23c1245978652706843857c5196
donghaozhang <zdhpeter1991@gmail.com>
2021-05-24 13:42:21 +1000

    I am still working on the retina dataset

--- clean_datasetv2.py
+++ clean_datasetv2.py
@@ -5,13 +5,15 @@ from random import random
 import re
 from collections import Counter
 
+# csv report path 
 cleaned_report_path = '/media/hdd/data/imcaption/danli_datav2/cleaned.csv'
+# image folder path
 img_folder_path = '/media/hdd/data/imcaption/danli_datav2/images819'
 cleaned_csv_list = []
+# 1 open csv file 2 iterate each row of csv file 3 save information of a single line 
 with open(cleaned_report_path) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
-
     for row in csv_reader:
         single_dic = {}
         if line_count == 0:
@@ -31,8 +33,6 @@ with open(cleaned_report_path) as csv_file:
             line_count += 1
             # print('The current path is', single_dic['dir'])
     print(f'Processed {line_count} lines.')
-# print('cur_dict[dir]', single_dic['dir'])
-# print(single_dic)
 
 without_list = []
 # print('the length of dictionary', len(cleaned_csv_list))
@@ -41,25 +41,19 @@ without_counter = 0
 for single_dic in cleaned_csv_list:
     if 'without' in  single_dic['text'] and len(single_dic['DS']) > 1:
         without_counter = without_counter + 1
-        # print(single_dic['id'], '!!!!!!!!')
-        # print(single_dic['text'])
         word_index = re.search(r'\b(without)\b', single_dic['text'])
         start_value = word_index.start()
-        # print('start_value', start_value, single_dic['text'][start_value:start_value+30])
-        # print('DS', single_dic['DS'])
         without_list.append(single_dic['text'][start_value:start_value+30])
         # if 'without any shadowing' in single_dic['text']:
         #     print(single_dic['text'], '!!!!!!')
         #     print('******')
         #     print(single_dic['DS'])
 print('the number of without', without_counter)
-# formulate the list into a dictionary
+# formulate the without list into a dictionary and count the apperances 
 without_cnt = Counter(without_list)
-# print(without_cnt)
 
-# for key, value in sorted(without_cnt.items(), key=lambda item:item[1], reverse=True):
-#     print(key, value)
 # manually checking whether the word is safe or not
+# without_unsafe_list is a list containing possible without phrase might cause errors 
 without_unsafe_list = ['without history of tuberous sclerosis']
 without_unsafe_list = without_unsafe_list + ['without any evidence']
 without_unsafe_list = without_unsafe_list + ['no history of glaucoma']
@@ -68,6 +62,8 @@ without_unsafe_list = without_unsafe_list + ['without any evidence of']
 without_unsafe_list = without_unsafe_list + ['without an intervening']
 without_unsafe_list = without_unsafe_list + ['without ocular symptoms']
 without_unsafe_list = without_unsafe_list + ['without the interposition of arterioles or capillaries']
+
+# without_safe_list is a list containing without phrase , but this phrase did not cause error for 'DS' 
 without_safe_list = ['without success.','without therapy.', 'without any care before.','without any problems.', 'without treatment', 'without cnvm', 'without known', 'without history of trauma', 'without symptom', 'without improvement']
 without_safe_list = without_safe_list + ['without significant']
 without_safe_list = without_safe_list + ['without pressure']
@@ -101,7 +97,6 @@ without_safe_list = without_safe_list + ['without intraocular lens']
 without_safe_list = without_safe_list + ['without specific treatment']
 without_safe_list = without_safe_list + ['without signs of cnvm']
 without_safe_list = without_safe_list + ['without any ophthalmological']
-# without_safe_list = without_safe_list + 
 without_safe_list = without_safe_list + ['without an apd']
 without_safe_list = without_safe_list + ['without any hx/fhx']
 without_safe_list = without_safe_list + ['without any signs']
@@ -109,24 +104,33 @@ without_safe_list = without_safe_list + ['without foveolar luster']
 without_safe_list = without_safe_list + ['without vitreous seeding']
 without_safe_list = without_safe_list + ['without any sr exudate']
 without_safe_list = without_safe_list + ['without any shadowing']
-# without_safe_list = without_safe_list + ['']
-# without_safe_list = without_safe_list + ['without foveolar']
-# without_safe_list = without_safe_list + 
-# without_safe_list = without_safe_list + 
-# without_safe_list = without_safe_list + ['']
-# without_safe_list = 
-# print('the length of without dictionary before removing words', len(without_cnt))
 without_cnt_copy = without_cnt.copy()
+
+# the following code is to check whether safe word has been removed
 for key, value in without_cnt.items():
     print_flag = True
     for safe_item in without_safe_list:
         if safe_item in key:
-            # print(key)
             del(without_cnt_copy[key])
             print_flag = False
-    if print_flag:
-        print(key, value)
-print('the length of without dictionary after removing words', len(without_cnt_copy))
-# {k: v for k, v in sorted(without_cnt.items(), key=lambda item: item[1])}without_safe_list
-print('without success' in 'without success. under general')
+    # if print_flag:
+    #     print(key, value)
+# print('the length of without dictionary after removing words', len(without_cnt_copy))
 
+
+# print the unsafe list
+unsafe_id_list = []
+safe_id_list = []
+print('the following ids might cause problems')
+for single_dic in cleaned_csv_list:
+    for unsafe_text in without_unsafe_list:
+        if unsafe_text in single_dic['text'] and len(single_dic['DS']) > 1:
+            unsafe_id_list.append(single_dic['id'])
+print(unsafe_id_list)
+
+print('the following ids are safe')
+for single_dic in cleaned_csv_list:
+    for safe_text in without_safe_list:
+        if safe_text in single_dic['text'] and len(single_dic['DS']) > 1:
+            safe_id_list.append(single_dic['id'])
+print(safe_id_list)
